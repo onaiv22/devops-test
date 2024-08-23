892afef743502757a5c0f334b900f000e6841dee
femi okuta <onaiv22@gmail.com>
2024-08-23 20:02:19 +0100

    fix region

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ module "subnets" {
    vpc_id     = module.vpc.vpc_id
    cidr_block = var.networks
    region     = var.aws_region
-   profile    = var.profile
+   # profile    = var.profile
    gw_id      = module.vpc.vpc_gw
 
 }
--- modules/networking/vars.tf
+++ modules/networking/vars.tf
@@ -1,7 +1,7 @@
 variable "region" {}
 variable "cidr_block" {}
 variable "vpc_id" {}
-variable "profile" {}
+# variable "profile" {}
 variable "networks" {
    type = map(any)
 }
